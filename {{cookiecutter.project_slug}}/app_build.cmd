82b4bd91b6dcacdce5eb6f08585c2f9340072d07
michalr <homeend@wp.pl>
2025-05-21 22:06:44 +0200

    update

--- {{cookiecutter.project_slug}}/app_build.cmd
+++ {{cookiecutter.project_slug}}/app_build.cmd
@@ -1,2 +1,6 @@
 docker compose build
-docker build -f Dockerfile --tag {{cookiecutter.project_slug}}:env .
+IF "%1"=="prod" (
+    docker build -f Dockerfile --tag {{cookiecutter.project_slug}}:prod .
+) ELSE (
+    docker build -f Dockerfile-dev --tag {{cookiecutter.project_slug}}:dev .
+)
